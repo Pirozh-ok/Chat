ba3a6fef70b61d578b93fbfbaaa7f5521033fd53
Pirozh-ok <70931484+Pirozh-ok@users.noreply.github.com>
2021-08-02 22:58:46 +0700

    Работаю над обновлением сообщений

--- Chat/MainFormChat.cs
+++ Chat/MainFormChat.cs
@@ -3,13 +3,15 @@ using System.Linq;
 using System.Windows.Forms;
 using System.Data.Entity;
 using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
 
 namespace Chat
 {
     public partial class MainFormChat : Form
     {
         public UserData UserAccount { get; set; }
-        private List <UserChat> ListChats { get; set; } = new List<UserChat>();
+        private List<UserChat> ListChats { get; set; } = new List<UserChat>();
         private UserChat CurrentChat { get; set; }
         public MainFormChat()
         {
@@ -18,7 +20,7 @@ namespace Chat
 
         private void MainFormChat_FormClosing(object sender, FormClosingEventArgs e)
         {
-            Application.Exit(); 
+            Application.Exit();
         }
 
         /* Инициализация формы данными аккаунта при её загрузке. */
@@ -34,9 +36,9 @@ namespace Chat
 
                 /*Получаем из базы данных список чатов, которые существуют у пользователя, сортированный по дате последнего сообщения в этом чате. */
                 ListChats = context.UserChats.Include(d => d.User)
-                                             .Where(x=>x.UserID == UserAccount.Id)
+                                             .Where(x => x.UserID == UserAccount.Id)
                                              .OrderByDescending(x => x.TimeLastMsg).ToList();
-                
+
                 if (ListChats.Count != 0)
                 {
                     lblNotChats.Visible = false;
@@ -46,39 +48,39 @@ namespace Chat
                     {
                         lbAllChat.Items.Add(chat.Name);
                     }
-                    this.lblSelectChat.Visible = true; 
+                    this.lblSelectChat.Visible = true;
                 }
             }
         }
 
         private void tbSearchChat_Click(object sender, EventArgs e)
         {
-            tbSearchChat.Text = string.Empty; 
+            tbSearchChat.Text = string.Empty;
         }
 
         private void bStartChat_Click(object sender, EventArgs e)
         {
             this.Hide();
             var newForm = new CreatNewChat();
-            newForm.UserAccount = this.UserAccount; 
+            newForm.UserAccount = this.UserAccount;
             newForm.Show();
         }
         private void bYourProfile_Click(object sender, EventArgs e)
         {
             var formProfile = new FormAboutMyProfile();
-            formProfile.UserAccount = this.UserAccount; 
+            formProfile.UserAccount = this.UserAccount;
             formProfile.ShowDialog();
         }
         private void tMsg_MouseClick(object sender, MouseEventArgs e)
         {
             if (tMsg.Text == "Enter message: ")
-                tMsg.Text = string.Empty; 
+                tMsg.Text = string.Empty;
         }
 
         private void tMsg_MouseLeave(object sender, EventArgs e)
         {
             if (tMsg.Text == string.Empty)
-                tMsg.Text = "Enter message: "; 
+                tMsg.Text = "Enter message: ";
         }
 
         private void tMsg_TextChanged(object sender, EventArgs e)
@@ -92,12 +94,12 @@ namespace Chat
         {
             using (var context = new DBContext())
             {
-                return msg.DateSend.ToShortTimeString() + " " + context.UserDatas.Single(x => x.Id == msg.IdSender).UserName + ": " + msg.Message; 
+                return msg.DateSend.ToShortTimeString() + " " + context.UserDatas.Single(x => x.Id == msg.IdSender).UserName + ": " + msg.Message;
             }
         }
         private void SendMsg()
         {
-            lblNotMsg.Visible = false; 
+            lblNotMsg.Visible = false;
 
             using (var context = new DBContext())
             {
@@ -107,11 +109,16 @@ namespace Chat
                     DateSend = DateTime.Now,
                     IdSender = UserAccount.Id,
                     IdRecipient = CurrentChat.IdRecipient
-                }; 
+                };
+
+                /* Обновляем время отправки последнего сообщений в чате. */
+                var currChat = context.UserChats.Single(x => x.Id == CurrentChat.Id);
+                currChat.TimeLastMsg = newMsg.DateSend;
+                CurrentChat = currChat;
 
                 context.Messages.Add(newMsg);
                 context.SaveChanges();
-                lbCurrentChat.Items.Add(FormatStringForMsg(newMsg)); 
+                lbCurrentChat.Items.Add(FormatStringForMsg(newMsg));
             }
 
             tMsg.Text = string.Empty;
@@ -120,7 +127,7 @@ namespace Chat
         private void tMsg_KeyDown(object sender, KeyEventArgs e)
         {
             if (e.KeyCode == Keys.Enter)
-                SendMsg(); 
+                SendMsg();
         }
 
         private void bSendMsg_Click(object sender, EventArgs e)
@@ -128,20 +135,25 @@ namespace Chat
             SendMsg();
         }
 
-        private void lbAllChat_SelectedIndexChanged(object sender, EventArgs e)
+        private async void lbAllChat_SelectedIndexChanged(object sender, EventArgs e)
         {
             if (lbAllChat.SelectedItem == null)
                 return;
 
-            lblSelectChat.Visible = false;
-            lbCurrentChat.Items.Clear(); 
+            await Task.Run(() => CheckNewMsg());
 
-            CurrentChat = ListChats[lbAllChat.SelectedIndex];
+            lblSelectChat.Visible = false;
+            lbCurrentChat.Items.Clear();
 
             using (var context = new DBContext())
             {
+                CurrentChat = context.UserChats.ToList().Single(x => x.Id == ListChats[lbAllChat.SelectedIndex].Id); 
+
                 var ListMsg = context.Messages.Where(x => x.IdSender == UserAccount.Id && x.IdRecipient == CurrentChat.IdRecipient
                                                        || x.IdSender == CurrentChat.IdRecipient && x.IdRecipient == UserAccount.Id).ToList();
+
+                /*var ListNewMsg = context.Messages.Where(x => (x.DateSend > CurrentChat.TimeLastMsg) && (x.IdSender == UserAccount.Id && x.IdRecipient == CurrentChat.IdRecipient
+                                                                   || x.IdSender == CurrentChat.IdRecipient && x.IdRecipient == UserAccount.Id)).ToList();*/
 
                 tMsg.Visible = true;
                 tMsg.Enabled = true;
@@ -154,7 +166,30 @@ namespace Chat
                 }
 
                 foreach (var msg in ListMsg)
+                {
                     lbCurrentChat.Items.Add(FormatStringForMsg(msg));
+                }
+            }
+        }
+
+        private void CheckNewMsg()
+        {
+            while (true)
+            {
+                Thread.Sleep(5000);
+                using (var context = new DBContext())
+                {
+                    var ListNewMsg = context.Messages.Where(x => (x.DateSend > CurrentChat.TimeLastMsg) 
+                                                        && (x.IdSender == UserAccount.Id && x.IdRecipient == CurrentChat.IdRecipient
+                                                         || x.IdSender == CurrentChat.IdRecipient && x.IdRecipient == UserAccount.Id)).ToList(); 
+                    
+                    if (ListNewMsg.Count != 0)
+                    {
+                        foreach (var msg in ListNewMsg)
+                            lbCurrentChat.Items.Add(FormatStringForMsg(msg));
+                    }
+
+                }
             }
         }
     }
